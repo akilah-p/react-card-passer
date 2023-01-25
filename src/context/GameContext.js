9c27b8e8b57870c86e17df2a7c1b832541c23eb5
Akilah Powell <atpjun@gmail.com>
2023-01-25 15:42:39 -0800

    added info in game context

---
+++ src/context/GameContext.js
@@ -0,0 +1,9 @@
+import {createContext, useState } from 'react';
+
+const GameContext = createContext();
+
+const GameProvider = ({ children }) => {
+    //this is going to be where state is defined
+};
+
+export {GameContext, GameProvider}
