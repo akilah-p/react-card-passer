85d7b99810a559e1d050900f733d5e099ae764a2
Akilah Powell <atpjun@gmail.com>
2023-01-26 08:26:03 -0800

    took brackets from initialCards

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -1,5 +1,5 @@
 import { createContext, useState } from 'react';
-import { initialCards } from '../cards-data.js';
+import initialCards from '../cards-data.js';
 
 const GameContext = createContext();
 
