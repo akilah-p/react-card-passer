425ce704ac1d3ef1029e8ea95136de67cd390fee
Akilah Powell <atpjun@gmail.com>
2023-01-25 16:03:26 -0800

    continued refactoring in GameContext

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -5,6 +5,30 @@ const GameContext = createContext();
 
 const GameProvider = ({ kids }) => {
     //this is going to be where state is defined
+  const [deck, setDeck] = useState(initialCards);
+  const [playerOneHand, setPlayerOneHand] = useState([]);
+  const [selectedCard, setSelectedCard] = useState();
+  const [playerTwoHand, setPlayerTwoHand] = useState([]);
+  const [playerThreeHand, setPlayerThreeHand] = useState([]);
+  const [from, setFrom] = useState('deck');
+  const [to, setTo] = useState(1);
+
+  return (
+    <GameContext.provider value={{
+      deck,
+      setDeck,
+      selectedCard,
+      setSelectedCard,
+      playerOneHand,
+      playerTwoHand,
+      playerThreeHand,
+      to,
+      setTo,
+    }}
+    >
+      {kids}
+    </GameContext.provider>
+  );
 };
 
 export { GameContext, GameProvider };
